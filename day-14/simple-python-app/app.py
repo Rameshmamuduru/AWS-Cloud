7edd0337b55c3cbb5c099f0be36cdc45cdc0aa50
Rameshmamuduru <rameshmamuduru7799@gmail.com>
2025-12-20 14:54:15 +0530

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Hello, world!..... This page is came from sample python app and got deployed by AWS Devops'
 
 if __name__ == '__main__':
     app.run()
+
 
